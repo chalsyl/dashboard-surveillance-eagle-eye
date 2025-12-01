47e958433a1eaf36abcb9d43f759dd56c9c7b8fa
chalsyl <156702451+chalsyl@users.noreply.github.com>
2025-12-01 20:56:57 +0000

    Add session management for user authentication

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit;
+}
 require_once 'database.php';
-
 // Récupération des statistiques
 $pdo = getDBConnection();
 
